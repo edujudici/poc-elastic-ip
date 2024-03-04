faa8ba1093baa23f667e2d3e41a90c663f3a6ce0
Eduardo Judici <eduardo.judici@ciandt.com>
2024-03-04 14:16:11 -0300

    feat: Start Lambda project

---
+++ hello-world/app.mjs
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event, context) => {
+  try {
+    const response = await axios.get("http://httpbin.org/ip");
+    const ip = response.data.origin;
+
+    return {
+      statusCode: 200,
+      body: `Seu IP é: ${ip}`,
+    };
+  } catch (error) {
+    console.error(`Erro ao acessar o serviço externo: ${error.message}`);
+
+    return {
+      statusCode: 500,
+      body: "Erro interno no servidor",
+    };
+  }
+};
